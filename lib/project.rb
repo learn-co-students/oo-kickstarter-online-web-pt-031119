55fea0f254dc3d28a18cafb98dc8e798d7ef5a55
Kelly Sciandra <kellysciandradev@icloud.com>
2019-04-10 20:01:50 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,22 @@
+class Project 
+  
+  attr_accessor :title, :backers 
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end 
+  
+def add_backer(backer)
+   backer.backed_projects << self
+   self.backers << backer
+ end
+  
+  
+  
+  
+  
+  
+  
+  
+end 
