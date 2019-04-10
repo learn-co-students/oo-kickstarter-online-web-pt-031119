bbef1cbe6ae3583e13aa9acd00378bbe3f48d5f8
James Cox <jamesncox@yahoo.com>
2019-04-10 20:28:24 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,19 @@
 class Backer
-attr_reader :name, :backed_projects
+  attr_reader :name, :backed_projects
+  # attr_accessor :name, :backed_projects
 
-def initialize(name)
-  @name = name
-  @backed_projects = []
-end
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
 
-def back_project(project)
-  backed_projects << project
-  project.backers << self
-end
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+
+  # def backed_projects(title)
+  #   @backed_projects << title
+  # end
   
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,14 @@
 class Project
-attr_reader :title, :backers
-  
-def initialize(title)
-  @title = title
-  @backers = []
-end
+  attr_reader :title, :backers
 
-def add_backer(backer)
-  backers << backer
-  backer.backed_projects << self
-end
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
   
 end
